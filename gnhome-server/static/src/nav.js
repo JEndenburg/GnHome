505852ce7ae7e9ac3f6af48631e0946370977a5b
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-05 16:17:16 +0100

    Collapsable nav bar

---
+++ gnhome-server/static/src/nav.js
@@ -0,0 +1,32 @@
+let _nav_collapsed = (localStorage.getItem("_nav_collapsed") || "true") === "true";
+
+function _nav_onCollapseButtonClicked(event) 
+{
+    _nav_collapsed = !_nav_collapsed;
+    localStorage.setItem("_nav_collapsed", _nav_collapsed);
+    _nav_updateNavBar();
+}
+
+function _nav_updateNavBar()
+{
+    const collapseButton = document.getElementById("nav-collapse");
+    if(_nav_collapsed)
+    {
+        document.documentElement.style.setProperty("--nav-offset", "calc(-20vw + 48px)");
+        collapseButton.style.setProperty("transform", "rotateY(0deg)");
+    }
+    else
+    {
+        document.documentElement.style.setProperty("--nav-offset", "0");
+        collapseButton.style.setProperty("transform", "rotateY(180deg)");
+    }
+}
+
+function _nav_init()
+{
+    const collapseButton = document.getElementById("nav-collapse");
+    collapseButton.addEventListener("click", _nav_onCollapseButtonClicked);
+    _nav_updateNavBar();
+}
+
+_nav_init();
