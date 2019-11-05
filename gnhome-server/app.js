bd5dd4aef27cf5cc3a0d093e5f53d59ff6b80766
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-05 12:49:13 +0100

    Basic setup for page rendering

--- gnhome-server/app.js
+++ gnhome-server/app.js
@@ -4,6 +4,7 @@ const interface = require("gnhome-interface");
 const APIGraphQL = require("./lib/api/graphql-api");
 const api = require("./lib/api/api");
 const port = 8000;
+const ejs = require("ejs");
 
 widgetLoader.loadWidgets(__dirname + "/widgets");
 
@@ -12,13 +13,30 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("static"));
 
-
-//TODO: Put Page rendering here.
 let graphQL = new APIGraphQL(app);
+
+app.use((req, res, next) => {
+    let path = "pages/" + req.path;
+
+    res.render(path, (error, html) => {
+        if(error == null)
+            res.send(html);
+        else
+        {
+            path += "/index";
+            res.render(path, (error2, html2) => {
+                if(error2 == null)
+                    res.send(html2);
+                else
+                    next();
+            })
+        }
+    });
+})
 
 //Fallback in case of 404
 app.use((req, res, next) => {
-    res.status(404).send("404 - Page not found");
+    res.status(404).send("404 - Page not found: " + req.path);
 });
 
 app.listen(port, () => console.log(`Now listening on port ${port}!`));
