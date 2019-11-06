2c2ede74d258ac9fcd0a0188824a8573ca7e12db
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-06 09:17:17 +0100

    Implemented dark mode, because yes, it's a must.

---
+++ gnhome-server/static/src/dark-mode-switch.js
@@ -0,0 +1,24 @@
+const _dm_switch = document.getElementById("dark-mode-switch").querySelector("input");
+_dm_switch.checked = (localStorage.getItem("_dm_darkmode") || "false") === "true";
+
+function _dm_onSwitchToggled(event)
+{
+    _dm_updateMode();
+}
+
+function _dm_updateMode()
+{
+    localStorage.setItem("_dm_darkmode", _dm_switch.checked);
+    document.getElementById("color-style").setAttribute("href", 
+        _dm_switch.checked ? "../style/colors/light.css" : "../style/colors/dark.css"
+    );
+}
+
+function _dm_init()
+{
+    _dm_updateMode();
+    _dm_switch.addEventListener("click", _dm_onSwitchToggled);
+    document.body.style.transition = "background-color 0.2s";
+}
+
+_dm_init();
