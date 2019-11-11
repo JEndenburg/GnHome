91e3b8280c2bb063f6667a9a5cb07fa2abc0125d
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-11 11:44:25 +0100

    Shoved compile bash script into production mode

--- gnhome-server/elm/compile.sh
+++ gnhome-server/elm/compile.sh
@@ -8,7 +8,12 @@ do
     inputFile="src\\${f}.elm"
     outputFile="output\\${f}.js"
     printf "Compiling \"${inputFile}\" to \"${outputFile}\".....\n"
-    elm make $inputFile --output $outputFile
+    elm make $inputFile --output $outputFile --optimize
+    printf "Compressing...\n"
+    uglifyjs $outputFile --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true,passes=2' --output=$outputFile
+    printf "Mangling...\n"
+    uglifyjs $outputFile --mangle --output=$outputFile
 done
 
-printf "Project compiled."
+printf "Project compiled.\n"
+read -p "Press [Enter] to exit."
---
+++ gnhome-server/elm/src/widgets/343/Main.elm
@@ -0,0 +1,7 @@
+port module Main exposing (..)
+
+import Browser
+import Html exposing (Html)
+import Http
+import Json.Decode as JSON exposing (Decoder)
+
