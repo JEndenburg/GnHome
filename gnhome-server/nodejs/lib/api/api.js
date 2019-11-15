4d17ecb7c62ea4992762913d22bbff3340148bc8
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-15 12:53:11 +0100

    Breaking all front-end logic over the fact that I'm turning all widget states user-specific.

--- gnhome-server/nodejs/lib/api/api.js
+++ gnhome-server/nodejs/lib/api/api.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const gnhomeInterface = require("gnhome-interface");
+const mariadb = require("mariadb");
+const fs = require("fs");
 const Response = gnhomeInterface.Response;
 const Size = gnhomeInterface.Widget.Size;
+
+const dbpool = mariadb.createPool(JSON.parse(fs.readFileSync(__dirname + "\\mariadb.credentials")));
 
 class API
 {
@@ -34,7 +38,6 @@ class API
             version: widget.version,
             status: this.parseResponseToObject(widget.status),
             size: this.parseSizeToObject(widget.size),
-            active: widget.active,
         };
     }
 
@@ -54,19 +57,209 @@ class API
     }
 
     /**
-     * @param {String} name
-     * @param {Boolean} value 
+     * @param {BigInt} userUUID 
      */
-    setWidgetActive(name, value)
+    async getUserById(userUUID)
     {
-        let success = false;
-        const widgetList = gnhomeInterface.getWidgetList();
-        if(widgetList[name])
+        let connection = null;
+        let response = {
+            statusCode: 500,
+            statusMessage: "Internal Server Error",
+            data: null,    
+        };
+
+        try
         {
-            widgetList[name].active = value;
-            success = true;
+            connection = await dbpool.getConnection();
+            let result = await connection.query("SELECT * FROM User WHERE id=" + userUUID);
+
+            if(result.length > 0)
+            {
+                let data = {
+                    uuid: result[0].id.toString(),
+                    firstName: result[0].first_name,
+                    lastName: result[0].last_name,
+                };
+
+                response = {
+                    statusCode: 200,
+                    statusMessage: "OK",
+                    data: data,
+                };
+            }
+            else
+            {
+                response = {
+                    statusCode: 404,
+                    statusMessage: "Not Found",
+                    data: null,
+                }
+            }
         }
-        return success;
+        catch(ex)
+        {
+        }
+        finally
+        {
+            if(connection != null)
+                await connection.end();
+        }
+
+        return response;
+    }
+
+    /**
+     * @param {BigInt} userUUID 
+     */
+    async getWidgetUserStates(userUUID)
+    {
+        let connection = null;
+        let response = {
+            statusCode: 500,
+            statusMessage: "Internal Server Error",
+            data: null,    
+        };
+
+        try
+        {
+            connection = await dbpool.getConnection();
+            let result = await connection.query("SELECT * FROM Widget_State WHERE owner_id=" + userUUID);
+
+            let data = [];
+
+            for(let i = 0; i < result.length; i++)
+            {
+                data[i] = {
+                    uuid: result[i].widget_id.toString(),
+                    position: { x: result[i].pos_x, y: result[i].pos_y, z: result[i].pos_z },
+                };
+            }
+
+            response = {
+                statusCode: 200,
+                statusMessage: "OK",
+                data: data,
+            };
+        }
+        catch(ex)
+        {
+        }
+        finally
+        {
+            if(connection != null)
+                await connection.end();
+        }
+
+        return response;
+    }
+
+    /**
+     * @param {BigInt} userUUID 
+     * @param {BigInt} widgetUUID 
+     * @param {Number} xPos
+     * @param {Number} yPos
+     * @param {Number} zPos
+     */
+    async setWidgetUserState(userUUID, widgetUUID, xPos, yPos, zPos)
+    {
+        let connection = null;
+        let response = {
+            statusCode: 500,
+            statusMessage: "Internal Server Error",
+        };
+
+        try
+        {
+            connection = await dbpool.getConnection();
+            let result;
+            if(await this.doesWidgetStateExist(userUUID, widgetUUID, connection))
+                result = await connection.query(`UPDATE Widget_State SET pos_x=${xPos}, pos_y=${yPos}, pos_z=${zPos} WHERE owner_id=${userUUID} AND widget_id=${widgetUUID}`);
+            else
+                result = await connection.query(`INSERT INTO Widget_State VALUES (${widgetUUID}, ${userUUID}, ${widgetUUID}, ${xPos}, ${yPos}, ${zPos})`);
+
+            if(result.affectedRows > 0)
+            {
+                response = {
+                    statusCode: 200,
+                    statusMessage: "OK",
+                };
+            }
+            else
+            {
+                response = {
+                    statusCode: 400,
+                    statusMessage: "Bad Request",
+                }
+            }
+        }
+        catch(ex)
+        {
+            console.log(ex);
+        }
+        finally
+        {
+            if(connection != null)
+                await connection.end();
+        }
+
+        return response;
+    }
+
+    /**
+     * @param {BigInt} userUUID 
+     * @param {BigInt} widgetUUID 
+     */
+    async deleteWidgetUserState(userUUID, widgetUUID)
+    {
+        let connection = null;
+        let response = {
+            statusCode: 500,
+            statusMessage: "Internal Server Error",
+        };
+
+        try
+        {
+            connection = await dbpool.getConnection();
+            let result;
+            result = await connection.query(`DELETE FROM Widget_State WHERE owner_id=${userUUID} AND widget_id=${widgetUUID}`);
+
+            if(result.affectedRows > 0)
+            {
+                response = {
+                    statusCode: 200,
+                    statusMessage: "OK",
+                };
+            }
+            else
+            {
+                response = {
+                    statusCode: 404,
+                    statusMessage: "Not Found",
+                }
+            }
+        }
+        catch(ex)
+        {
+            console.log(ex);
+        }
+        finally
+        {
+            if(connection != null)
+                await connection.end();
+        }
+
+        return response;
+    }
+
+    /**
+     * @param {BigInt} userUUID 
+     * @param {BigInt} widgetUUID 
+     * @param {mariadb.PoolConnection} connection
+     */
+    async doesWidgetStateExist(userUUID, widgetUUID, connection)
+    {
+        let result = await connection.query(`SELECT * FROM Widget_State WHERE owner_id=${userUUID} AND widget_id=${widgetUUID}`);
+        return (result.length > 0)
     }
 
     /**
--- gnhome-server/nodejs/lib/api/graphql-api.js
+++ gnhome-server/nodejs/lib/api/graphql-api.js
@@ -7,13 +7,16 @@ const schema = buildSchema(`
 
 type Mutation
 {
-    setWidget(name: String!, active: Boolean!): Boolean
+    setWidgetState(userUUID: String! widgetUUID: String! x: Float! y: Float! z: Float!): MutationResponse
+    deleteWidgetState(userUUID: String! widgetUUID: String!): MutationResponse
 }
 
 type Query
 {
     main: PDA!
     getWidget(name: String!): Widget
+    getUser(uuid: String!): UserResponse
+    getWidgetState(userUUID: String!) : WidgetStateResponse
 }
 
 type PDA
@@ -29,7 +32,6 @@ type Widget
     description: String
     status: Response
     size: Size
-    active: Boolean
 }
 
 type Response
@@ -43,6 +45,46 @@ type Size
 {
     width: Int
     height: Int
+}
+
+type UserResponse
+{
+    statusCode: Int
+    statusMessage: String
+    data: User
+}
+
+type User
+{
+    uuid: String
+    firstName: String
+    lastName: String
+}
+
+type WidgetStateResponse
+{
+    statusCode: Int
+    statusMessage: String
+    data: [WidgetState]
+}
+
+type WidgetState
+{
+    uuid: String
+    position: Position
+}
+
+type Position
+{
+    x: Float
+    y: Float
+    z: Float
+}
+
+type MutationResponse
+{
+    statusCode: Int
+    statusMessage: String
 }
 
 `);
@@ -66,9 +108,12 @@ class APIGraphQL extends API
     get root()
     {
         return {
-            setWidget: (args) => super.setWidgetActive(args.name, args.active),
             getWidget: (args) => super.getWidgetByName(args.name),
             main: (args) => super.getPDA(),
+            getUser: (args) => super.getUserById(BigInt(args.uuid)),
+            getWidgetState: (args) => super.getWidgetUserStates(BigInt(args.userUUID)),
+            setWidgetState: (args) => super.setWidgetUserState(BigInt(args.userUUID), BigInt(args.widgetUUID), args.x, args.y, args.z),
+            deleteWidgetState: (args) => super.deleteWidgetUserState(BigInt(args.userUUID), BigInt(args.widgetUUID)),
         }
     }
 }
