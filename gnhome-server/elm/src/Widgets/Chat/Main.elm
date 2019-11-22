0fe36cb8aa0801c18f324a20e92ab8a5b203050c
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-22 15:30:25 +0100

    Websocket message processing basics

--- gnhome-server/elm/src/Widgets/Chat/Main.elm
+++ gnhome-server/elm/src/Widgets/Chat/Main.elm
@@ -2,25 +2,25 @@ module Widgets.Chat.Main exposing (..)
 
 import Browser exposing (Document)
 
-import Html exposing (Html, text, div)
+import Html exposing (Html, text, div, span)
 import Html.Attributes exposing (class, id)
 import Html.Events
 
-import Json.Decode as JSON exposing (Decoder)
+import Json.Decode as JSON exposing (field, at, string, list)
 
-type alias Model =
-    {   username : String
-    ,   messages : List Message
-    }
+import Time exposing (Posix, toYear, toMonth, toDay, toHour, toMinute, toSecond, Month(..))
 
-type alias Message =
-    {   username : String
-    ,   content : String
-    }
+import Widgets.Chat.Socket as Socket exposing(Message)
+
+type Model
+    = Connecting
+    | Connected Socket.Viewer
+    | Disconnected
+    | Error
 
 type Event
-    = OnSendMessage
-
+    = OnReceiveSocketMessage Socket.Event
+    | OnSendMessage
 
 
 main = Browser.document
@@ -32,24 +32,86 @@ main = Browser.document
 
 
 init : JSON.Value -> (Model, Cmd Event)
-init flags = 
-    (   {   username = "nobody"
-        ,   messages = []
-        }
-    ,   Cmd.none
-    )
+init _ = (Connecting, Cmd.none)
 
 view : Model -> Document Event
 view model = 
     {   title = "WebChat!"
-    ,   body = []
+    ,   body = 
+        [   case model of
+                Connecting -> viewConnecting
+                Disconnected -> viewDisconnected
+                Connected viewer -> viewConnected viewer
+                Error -> Debug.todo "Implement Error view"
+        ]
     }
 
 update : Event -> Model -> (Model, Cmd Event)
-update event model = (model, Cmd.none)
+update event model =
+    case event of
+        OnReceiveSocketMessage socketEvent -> case Socket.update socketEvent of
+            Socket.Connected viewer -> let _ = Debug.log "Connected" () in (model, Cmd.none)
+            Socket.Disconnected ->  let _ = Debug.log "Disconnected" () in (model, Cmd.none)
+            Socket.ReceivedMessage message -> let _ = Debug.log "Received Message" message in (model, Cmd.none)
+            Socket.Invalid -> let _ = Debug.log "Invalid!" () in (model, Cmd.none)
+        OnSendMessage -> Debug.todo "Implement sending message"
 
 subscriptions : Model -> Sub Event
-subscriptions model =
-    Sub.none
+subscriptions model = Sub.batch
+    [   Sub.map OnReceiveSocketMessage Socket.subscriptions
+    ]
 
 
+viewConnecting : Html Event
+viewConnecting =
+    div[] [text "Loading..."]
+
+viewDisconnected : Html Event
+viewDisconnected = 
+    div[] [text "Failed!"]
+
+viewConnected : Socket.Viewer -> Html Event
+viewConnected state = 
+    div[]
+    [   div[] [text ("User: " ++ state.username)]
+    ,   div[] (List.map viewMessage state.messages)
+    ]
+
+viewMessage : Message -> Html Event
+viewMessage message = 
+    div[]
+    [   div[] [text (message.author ++ " sent:")]
+    ,   div[] [text message.content]
+    ,   viewTimestamp message.time
+    ]
+
+viewTimestamp : Posix -> Html Event
+viewTimestamp posix =
+    span [class "timestamp"] [ text 
+        (   (String.fromInt (toYear Time.utc posix)) 
+        ++  "-"
+        ++  (String.fromInt (monthToInt (toMonth Time.utc posix)))
+        ++  "-"
+        ++  (String.fromInt (toDay Time.utc posix))
+        ++  " "
+        ++  (String.fromInt (toHour Time.utc posix))
+        ++  ":"
+        ++  (String.fromInt (toMinute Time.utc posix))
+        ++ " (UTC)"
+        ) ]
+
+monthToInt : Time.Month -> Int
+monthToInt month = 
+    case month of
+        Jan -> 1
+        Feb -> 2
+        Mar -> 3
+        Apr -> 4
+        May -> 5
+        Jun -> 6
+        Jul -> 7
+        Aug -> 8
+        Sep -> 9
+        Oct -> 10
+        Nov -> 11
+        Dec -> 12
---
+++ gnhome-server/elm/src/Widgets/Chat/Socket.elm
@@ -0,0 +1,103 @@
+port module Widgets.Chat.Socket exposing (..)
+
+import Json.Decode as JSON exposing(Decoder, field, int, string, null)
+import Json.Decode.Extra exposing (datetime)
+
+import Time exposing (Posix, toYear, toMonth, toDay, toHour, toMinute, toSecond, Month(..))
+
+
+type SocketMessageType
+    = Connected Viewer
+    | Disconnected
+    | ReceivedMessage Message
+    | Invalid
+
+type alias SocketMessage =
+    {   identifier : Int
+    ,   content : Maybe SocketMessageContent
+    }
+
+type alias SocketMessageContent =
+    {   message : Maybe Message
+    ,   state : Maybe Viewer
+    }
+
+type alias Message =
+    {   author : String
+    ,   time : Posix
+    ,   content : String
+    }
+
+type alias Viewer =
+    {   username : String
+    ,   messages : List Message
+    }
+
+type Event
+    = OnSocketMessageReceived JSON.Value
+
+
+port receiveSocketMessage : (JSON.Value -> msg) -> Sub msg
+
+
+subscriptions : Sub Event
+subscriptions = Sub.batch
+    [   receiveSocketMessage OnSocketMessageReceived
+    ]
+
+update : Event -> SocketMessageType
+update event =
+    case event of
+        OnSocketMessageReceived json -> decodeSocketMessage json
+
+
+decodeSocketMessage : JSON.Value -> SocketMessageType
+decodeSocketMessage json = 
+    case JSON.decodeValue socketMessageDecoder json of
+        Ok message -> case message.identifier of
+            0 -> Disconnected
+            1 -> case message.content of
+                Just content -> case content.state of
+                    Just viewer -> Connected viewer
+                    Nothing -> Invalid
+                Nothing -> Invalid
+            2 -> case message.content of
+                Just content -> case content.message of
+                    Just userMessage -> ReceivedMessage userMessage
+                    Nothing -> Invalid
+                Nothing -> Invalid
+            _ -> Invalid
+        Err _ -> Invalid
+
+socketMessageDecoder : JSON.Decoder SocketMessage
+socketMessageDecoder = 
+    JSON.map2 SocketMessage
+        (field "id" int)
+        (
+            field "id" int
+            |> JSON.andThen (socketMessageContentDecoder)
+        )
+
+socketMessageContentDecoder : Int -> JSON.Decoder (Maybe SocketMessageContent)
+socketMessageContentDecoder id =
+    case id of
+        1 -> JSON.maybe <| JSON.map2 SocketMessageContent
+                (JSON.succeed Nothing)
+                (JSON.maybe (field "message" viewerDecoder))
+        2 -> JSON.maybe <| JSON.map2 SocketMessageContent
+                (JSON.maybe (field "message" messageDecoder))
+                (JSON.succeed Nothing)
+        _ -> JSON.succeed Nothing
+
+messageDecoder : JSON.Decoder Message
+messageDecoder = 
+    JSON.map3 Message
+        (field "author" string)
+        (field "timestamp" datetime)
+        (field "content" string)
+
+viewerDecoder : JSON.Decoder Viewer
+viewerDecoder =
+    JSON.map2 Viewer
+        (field "username" string)
+        (field "history" (JSON.list messageDecoder))
